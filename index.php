2dfa89a41ff752a5313b2c77794a28b907145c87
Rtd21 <135415553+Rtd21@users.noreply.github.com>
2026-05-07 11:47:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include 'koneksi.php'; ?>
+<?php include 'crud.php'; ?>
 
 <!DOCTYPE html>
 <html>
@@ -6,55 +6,90 @@
     <title>PHP CRUD Railway</title>
 </head>
 <body>
-    <h2>Tambah Data</h2>
-    <form method="POST">
-        <input type="text" name="nama" placeholder="Nama" required>
-        <input type="sandi" name="sandi" placeholder="sandi" required>
+
+<?php
+$edit = false;
+$id = "";
+$nama = "";
+$sandi = "";
+
+if(isset($_GET['edit'])){
+    $edit = true;
+
+    $id_edit = $_GET['edit'];
+
+    $ambil = mysqli_query($koneksi, "SELECT * FROM users WHERE id='$id_edit'");
+    $row = mysqli_fetch_assoc($ambil);
+
+    $id = $row['id'];
+    $nama = $row['nama'];
+    $sandi = $row['sandi'];
+}
+?>
+
+<h2>
+    <?php echo $edit ? "Edit Data" : "Tambah Data"; ?>
+</h2>
+
+<form method="POST">
+
+    <input type="hidden" name="id" value="<?php echo $id; ?>">
+
+    <input type="text"
+           name="nama"
+           placeholder="Nama"
+           required
+           value="<?php echo $nama; ?>">
+
+    <input type="text"
+           name="sandi"
+           placeholder="Sandi"
+           required
+           value="<?php echo $sandi; ?>">
+
+    <?php if($edit){ ?>
+        <button type="submit" name="update">Update</button>
+    <?php } else { ?>
         <button type="submit" name="tambah">Simpan</button>
-    </form>
+    <?php } ?>
 
-    <?php
+</form>
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+<h2>Data Users</h2>
 
-    // Logika Create
-    if(isset($_POST['tambah'])){
-        $nama = $_POST['nama'];
-        $sandi = $_POST['sandi'];
-        mysqli_query($koneksi, "INSERT INTO users (nama, sandi) VALUES('$nama', '$sandi')");
-    }
+<table border="1" cellpadding="10">
 
-    // Logika Delete
-    if(isset($_GET['hapus'])){
-        $id = $_GET['hapus'];
-        mysqli_query($koneksi, "DELETE FROM users WHERE id=$id");
-        header("Location: index.php");
-    }
-    ?>
+    <tr>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>Sandi</th>
+        <th>Aksi</th>
+    </tr>
 
-    <h2>Data Users</h2>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Nama</th>
-            <th>sandi</th>
-            <th>Aksi</th>
-        </tr>
-        <?php
-        $data = mysqli_query($koneksi, "SELECT * FROM users");
-        while($d = mysqli_fetch_array($data)){
-        ?>
-        <tr>
-            <td><?php echo $d['id']; ?></td>
-            <td><?php echo $d['nama']; ?></td>
-            <td><?php echo $d['sandi']; ?></td>
-            <td>
-                <a href="index.php?hapus=<?php echo $d['id']; ?>">Hapus</a>
-            </td>
-        </tr>
-        <?php } ?>
-    </table>
+    <?php while($d = mysqli_fetch_array($data)){ ?>
+
+    <tr>
+        <td><?php echo $d['id']; ?></td>
+        <td><?php echo $d['nama']; ?></td>
+        <td><?php echo $d['sandi']; ?></td>
+
+        <td>
+            <a href="index.php?edit=<?php echo $d['id']; ?>">
+                Edit
+            </a>
+
+            |
+
+            <a href="index.php?hapus=<?php echo $d['id']; ?>"
+               onclick="return confirm('Yakin hapus data?')">
+                Hapus
+            </a>
+        </td>
+    </tr>
+
+    <?php } ?>
+
+</table>
+
 </body>
 </html>
